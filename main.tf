30f6a686e7405999e26953481cfdfc2292806326
Zsolt Molnar <ysolt@ysys.hu>
2025-02-24 15:33:10 +0100

    rename accelerated networking flag

--- main.tf
+++ main.tf
@@ -508,7 +508,7 @@ resource "azurerm_network_interface" "vm" {
   location                      = local.location
   name                          = replace(replace(var.name_template_network_interface, "$${vm_hostname}", var.vm_hostname), "$${host_number}", count.index)
   resource_group_name           = var.resource_group_name
-  enable_accelerated_networking = var.enable_accelerated_networking
+  accelerated_networking_enabled = var.enable_accelerated_networking
   enable_ip_forwarding          = var.enable_ip_forwarding
   tags = merge(var.tags, (/*<box>*/ (var.tracing_tags_enabled ? { for k, v in /*</box>*/ {
     avm_git_commit           = "78b54d36f6674612398cab6b85f1a149543d5ade"
